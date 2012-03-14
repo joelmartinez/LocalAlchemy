135ae863543a0e909de82e6507471e6cad666ca0
Joel Martinez <joelmartinez@gmail.com>
2012-03-13 22:22:00 -0400

    adding WindowsParser

---
+++ LocalAlchemy/WindowsParser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LocalAlchemy
+{
+    public class WindowsParser : Parser
+    {
+        public WindowsParser()
+            : base(".resx")
+        {
+        }
+
+        public override IEnumerable<TranslateUnit> Parse(string file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(string sourcefile, string targetLang, IEnumerable<TranslateUnit> translated)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
